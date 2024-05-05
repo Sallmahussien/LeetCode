2f0411a04cdadb120269aed486b7bad7d0d452bd
Salma Hussien <salmaa.hussien1@gmail.com>
2024-05-05 12:05:28 +0300

    [169] [Time Beats: 68.59%] [Memory Beats: 48.75%] - LeetPush

--- 169-Majority-Element.cpp
+++ 169-Majority-Element.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int, int> freq_arr;
-        int n = nums.size() / 2;
-        int res;
+        // using hash map
+        // map<int, int> freq_arr;
+        // int n = nums.size() / 2;
+        // int res;
 
+        // for (int num: nums) {
+        //     freq_arr[num]++;
+        //     if (freq_arr[num] > n) {
+        //         res = num;
+        //         break;
+        //     }
+        // }
+
+        // return res;
+
+        // using boyer moore algo
+        int res = 0, count = 0;
         for (int num: nums) {
-            freq_arr[num]++;
-            if (freq_arr[num] > n) {
-                res = num;
-                break;
-            }
+            if (count == 0) res = num;
+
+            count += (res == num) ? 1 : -1;
         }
 
         return res;
