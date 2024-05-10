1d5778e530ab03f62923d48b43ec413b0660dfb2
Salma Hussien <salmaa.hussien1@gmail.com>
2024-05-10 11:39:46 +0300

    [242] [Time Beats: 30.18%] [Memory Beats: 32.55%] - LeetPush

---
+++ 242-Valid-Anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char, int> count;
+
+        for (char c: s) {
+            count[c]++;
+        }
+
+        for (char c: t) {
+            count[c]--;
+        }
+
+        for (auto x: count) {
+            if (x.second != 0) return false;
+        }
+
+        return true;
+    }
+};
