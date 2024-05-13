34bbfa0c5ab734ddf12dfa5bf51a69a4a244f6a3
Salma Hussien <salmaa.hussien1@gmail.com>
2024-05-13 16:46:42 +0300

    [80] [Time Beats: 30.24%] [Memory Beats: 40.45%] - LeetPush

---
+++ 80-Remove-Duplicates-from-Sorted-Array-II.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    int _min(int num1, int num2) {
+        if (num1 < num2) return num1;
+        return num2;
+    }
+
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int l = 0, r = 0;
+
+        while(r < nums.size()) {
+            int count = 1;
+
+            while (r + 1 < nums.size() && nums[r] == nums[r+1]) count++, r++;
+
+            for (int i = 0; i < min(2, count); i++) {
+                nums[l] = nums[r];
+                l++;
+            }
+
+            r++;
+        }
+
+        return l;
+    }
+};
