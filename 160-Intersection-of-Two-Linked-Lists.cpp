bcad4fac22b41cfc1099eeb4d8769d3035fb9345
Salma Hussien <salmaa.hussien1@gmail.com>
2024-05-14 16:35:08 +0300

    [160] [Time Beats: 61.13%] [Memory Beats: 68.83%] - LeetPush

--- 160-Intersection-of-Two-Linked-Lists.cpp
+++ 160-Intersection-of-Two-Linked-Lists.cpp
@@ -7,26 +7,66 @@
  * };
  */
 class Solution {
+private:
+    int get_length(ListNode* head) {
+        ListNode* curr = head;
+        int count = 0;
+
+        while(curr) {
+            count++;
+            curr = curr->next;
+        }
+
+        return count;
+    }
 public:
     ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
-        if (headA == headB) return headA;
+        // solution using set
+        // if (headA == headB) return headA;
+
+        // ListNode* curr1 = headA;
+        // ListNode* curr2 = headB;
+        // unordered_set<ListNode*> addresses;
+
+        // while (curr1 || curr2) {
+        //     if (curr1) {
+        //         if (addresses.count(curr1)) return curr1;
+        //         addresses.insert(curr1);
+        //         curr1 = curr1->next;
+        //     }
+
+        //     if (curr2) {
+        //         if (addresses.count(curr2)) return curr2;
+        //         addresses.insert(curr2);
+        //         curr2 = curr2->next;
+        //     }
+        // }
+
+        // return nullptr;
+
+        // solution 2
+        int len1 = get_length(headA);
+        int len2 = get_length(headB);
 
         ListNode* curr1 = headA;
         ListNode* curr2 = headB;
-        unordered_set<ListNode*> addresses;
 
-        while (curr1 || curr2) {
-            if (curr1) {
-                if (addresses.count(curr1)) return curr1;
-                addresses.insert(curr1);
+        if (len1 > len2) {
+            while(len1 != len2) {
                 curr1 = curr1->next;
+                len1--;
             }
-
-            if (curr2) {
-                if (addresses.count(curr2)) return curr2;
-                addresses.insert(curr2);
+        } else {
+            while(len1 != len2) {
                 curr2 = curr2->next;
+                len2--;
             }
+        }
+
+        while (curr1 && curr2) {
+            if (curr1 == curr2) return curr1;
+            curr1 = curr1->next;
+            curr2 = curr2->next; 
         }
 
         return nullptr;
