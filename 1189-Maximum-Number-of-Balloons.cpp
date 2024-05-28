60cb0e393004904a626bb9e2005f57fb17ef1da2
Salma Hussien <salmaa.hussien1@gmail.com>
2024-05-28 17:00:10 +0300

    [1189] [Time Beats: 23.14%] [Memory Beats: 12.55%] - LeetPush

---
+++ 1189-Maximum-Number-of-Balloons.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        string balloon = "balloon";
+        unordered_map<char, int> freq_arr;
+
+        for (char c: balloon) {
+            freq_arr[c] = 0; 
+        }
+
+        for (char c: text) {
+            if (freq_arr.count(c)) freq_arr[c]++;
+        }
+
+        freq_arr['l'] /= 2;
+        freq_arr['o'] /= 2;
+
+        int min = INT_MAX;
+
+        for (auto& pair: freq_arr) {
+            if (pair.second < min) min = pair.second;
+        }
+
+        return min;   
+    }
+};
