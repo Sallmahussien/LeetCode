7888f50a4be92f5364503a8f5137f1905d1fb986
Salma Hussien <salmaa.hussien1@gmail.com>
2024-06-10 11:51:39 +0300

    [1051] [Time Beats: 100.00%] [Memory Beats: 44.04%] - LeetPush

---
+++ 1051-Height-Checker.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> sorted_heights = heights;
+        sort(sorted_heights.begin(), sorted_heights.end());
+        int res = 0;
+
+        for (int i = 0; i < heights.size(); i++) {
+            if (heights[i] != sorted_heights[i]) res += 1;
+        }
+
+        return res;
+    }
+};
