552bc55341611755e0d7aac0774bb2af4b917fa7
Salma Hussien <salmaa.hussien1@gmail.com>
2024-06-14 18:08:56 +0300

    [945] [Time Beats: undefined] [Memory Beats: undefined] - LeetPush

---
+++ 945-Minimum-Increment-to-Make-Array-Unique.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        int res = 0;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] <= nums[i-1]) {
+                res += (nums[i-1] + 1) - nums[i];
+                nums[i] = nums[i-1] + 1;
+            }
+        }
+
+        return res;
+    }
+};
