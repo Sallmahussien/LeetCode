babcac1eb6e8478f362e39b437ef411fecebb8b8
Salma Hussien <salmaa.hussien1@gmail.com>
2024-06-18 12:08:08 +0300

    [633] [Time Beats: undefined] [Memory Beats: undefined] - LeetPush

--- 633-Sum-of-Square-Numbers.cpp
+++ 633-Sum-of-Square-Numbers.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        long long a = 0;
+        // long long a = 0;
 
-        while (a * a <= c) {
-            double b = sqrt(c - (a * a));
-            if (b == (int)b) return true;
+        // while (a * a <= c) {
+        //     double b = sqrt(c - (a * a));
+        //     if (b == (int)b) return true;
 
-            a += 1;
+        //     a += 1;
+        // }
+
+        // return false;
+
+
+        int l = 0, r = sqrt(c);
+        while(l <= r) {
+            long long result = pow(l, 2) + pow(r, 2);
+            if (result == c) return true;
+
+            if (result > c) r--;
+            else l++;
         }
 
         return false;
