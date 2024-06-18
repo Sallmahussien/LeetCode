13b0caf3e3a6d10e579c5eaf25cab3f027021cf9
Salma Hussien <salmaa.hussien1@gmail.com>
2024-06-18 13:52:43 +0300

    [826] [Time Beats: undefined] [Memory Beats: undefined] - LeetPush

--- 826-Most-Profit-Assigning-Work.cpp
+++ 826-Most-Profit-Assigning-Work.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        int n = profit.size();
-        vector<pair<int, int>> jobs(n);
-        for (int i = 0; i < n; i++) {
-            jobs[i] = {difficulty[i], profit[i]};
+        // int n = profit.size();
+        // vector<pair<int, int>> jobs(n);
+        // for (int i = 0; i < n; i++) {
+        //     jobs[i] = {difficulty[i], profit[i]};
+        // }
+
+        // sort(jobs.begin(), jobs.end());
+        // sort(worker.begin(), worker.end());
+
+        // int maxProfit = 0, totalProfit = 0, idx = 0;
+        // for (int w : worker) {
+        //     while (idx < n && w >= jobs[idx].first) {
+        //         maxProfit = max(maxProfit, jobs[idx].second);
+        //         idx++;
+        //     }
+
+        //     totalProfit += maxProfit;
+        // }
+
+        // return totalProfit;
+
+        unordered_map<int, int> jobs;
+        for (int i = 0; i < profit.size(); i++) {
+            jobs[difficulty[i]] = max(jobs[difficulty[i]], profit[i]);
         }
 
-        sort(jobs.begin(), jobs.end());
+        sort(difficulty.begin(), difficulty.end());
         sort(worker.begin(), worker.end());
+        int totalProfit = 0, maxProfit = 0, idx = 0;
 
-        int maxProfit = 0, totalProfit = 0, idx = 0;
         for (int w : worker) {
-            while (idx < n && w >= jobs[idx].first) {
-                maxProfit = max(maxProfit, jobs[idx].second);
-                idx++;
+            
+            for (; idx < difficulty.size() && w >= difficulty[idx]; idx++)  {
+                maxProfit = max(maxProfit, jobs[difficulty[idx]]);
             }
 
             totalProfit += maxProfit;
