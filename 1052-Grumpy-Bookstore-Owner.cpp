de1a99c7cd5a36ceb1dfef20dafb0a631b592f87
Salma Hussien <salmaa.hussien1@gmail.com>
2024-06-21 18:08:40 +0300

    [1052] [Time Beats: 66.42%] [Memory Beats: 47.97%] - LeetPush

--- 1052-Grumpy-Bookstore-Owner.cpp
+++ 1052-Grumpy-Bookstore-Owner.cpp
@@ -8,13 +8,10 @@ public:
         }
 
         int additionalCustomers = 0, currWindow = 0;
-        int count = 0;
         for (int i = 0; i < customers.size(); i++) {
             if (grumpy[i]) currWindow += customers[i];
-            count++;
 
-            if (count == minutes) {
-                count--;
+            if (i+1 >= minutes) {
                 additionalCustomers = max(additionalCustomers, currWindow);
                 if (grumpy[i+1-minutes]) currWindow -= customers[i+1-minutes];
             }
