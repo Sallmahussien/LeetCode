7e26d763fd122266ab89b9553a63ca13b5970323
Salma Hussien <salmaa.hussien1@gmail.com>
2024-06-26 14:11:58 +0300

    [896] [Time Beats: 55.07%] [Memory Beats: 89.10%] - LeetPush

---
+++ 896-Monotonic-Array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    bool isIncreasing(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < nums[i-1]) return false;
+        }
+
+        return true;
+    }
+
+    bool isDecreasing(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > nums[i-1]) return false;
+        }
+
+        return true;
+    }
+public:
+    bool isMonotonic(vector<int>& nums) {
+        return isIncreasing(nums) || isDecreasing(nums);
+    }
+};
