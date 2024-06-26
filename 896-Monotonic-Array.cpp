10d959d76f254e3ac283722057b692f13dac4eea
Salma Hussien <salmaa.hussien1@gmail.com>
2024-06-26 14:14:49 +0300

    [896] [Time Beats: 63.31%] [Memory Beats: 28.04%] - LeetPush

--- 896-Monotonic-Array.cpp
+++ 896-Monotonic-Array.cpp
@@ -17,6 +17,15 @@ private:
     }
 public:
     bool isMonotonic(vector<int>& nums) {
-        return isIncreasing(nums) || isDecreasing(nums);
+        // return isIncreasing(nums) || isDecreasing(nums);
+
+        bool isInc = true, isDec = true;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < nums[i-1]) isInc = false;
+            if (nums[i] > nums[i-1]) isDec = false;
+        }
+
+        return isInc || isDec;
     }
 };
