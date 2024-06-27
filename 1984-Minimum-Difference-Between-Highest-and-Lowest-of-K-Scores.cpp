3e219bf3f5290a04a0871a364b6c287bd1d4d80a
Salma Hussien <salmaa.hussien1@gmail.com>
2024-06-27 12:22:27 +0300

    [1984] [Time Beats: 56.40%] [Memory Beats: 57.07%] - LeetPush

--- 1984-Minimum-Difference-Between-Highest-and-Lowest-of-K-Scores.cpp
+++ 1984-Minimum-Difference-Between-Highest-and-Lowest-of-K-Scores.cpp
@@ -1,21 +1,12 @@
 class Solution {
-private:
-    void print_arr(vector<int>& arr) {
-        for (int num: arr) cout << num << " ";
-
-        cout << endl;
-    }
-
 public:
     int minimumDifference(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
-        int min = INT_MAX;
-
-        for (int i = 0; i <= nums.size() - k; i++) {
-            int diff = nums[i+k-1] - nums[i];
-            if (min > diff) min = diff;
+        int min_diff = INT_MAX;
+        for (int i = 0; i < nums.size()-k+1; i++) {
+            min_diff = min(min_diff, nums[i+k-1] - nums[i]);
         }
 
-        return min;        
+        return min_diff;
     }
 };
