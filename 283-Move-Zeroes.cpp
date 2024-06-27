717b3a3f66b898ee11520ad8a42860acc82c0cd7
Salma Hussien <salmaa.hussien1@gmail.com>
2024-06-27 13:18:27 +0300

    [283] [Time Beats: 68.34%] [Memory Beats: 73.70%] - LeetPush

--- 283-Move-Zeroes.cpp
+++ 283-Move-Zeroes.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i = 0, j = 1;
+        // int i = 0, j = 1;
 
-        while (j < nums.size()) {
-            if (nums[i] == 0) {
-                while(j < nums.size() && nums[j] == 0) j++;
-                if (j == nums.size()) break;
-                nums[i] = nums[j];
-                nums[j] = 0;
+        // while (j < nums.size()) {
+        //     if (nums[i] == 0) {
+        //         while(j < nums.size() && nums[j] == 0) j++;
+        //         if (j == nums.size()) break;
+        //         nums[i] = nums[j];
+        //         nums[j] = 0;
+        //     }
+        //     i++;
+        //     j++;
+        // }
+
+        
+        int l = 0, r = 0;
+        for (; l < nums.size() && r < nums.size(); l++, r++) {
+            if (nums[l] == 0) {
+                while (r < nums.size() && nums[r] == 0) r++;
             }
-            i++;
-            j++;
-        }        
+
+            if (r == nums.size()) break;
+            swap(nums[l], nums[r]);
+        }
     }
 };
