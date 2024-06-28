7e53dbb99289e144de482b20f885119d5eb71fd7
Salma Hussien <salmaa.hussien1@gmail.com>
2024-06-28 22:31:48 +0300

    [19] [Time Beats: 100.00%] [Memory Beats: 92.71%] - LeetPush

--- 19-Remove-Nth-Node-From-End-of-List.cpp
+++ 19-Remove-Nth-Node-From-End-of-List.cpp
@@ -11,35 +11,56 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* lastNode = head;
-        int len = 1;
+        // ListNode* lastNode = head;
+        // int len = 1;
 
-        while (lastNode->next) {
-            len++;
-            lastNode = lastNode->next;
+        // while (lastNode->next) {
+        //     len++;
+        //     lastNode = lastNode->next;
+        // }
+
+        // if (len == n) {
+        //     head = head->next;
+        //     return head;
+        // }
+
+        // if (n > len) return head;
+
+        // ListNode* prev = nullptr;
+        // ListNode* curr = head;
+        // int count = 0;
+
+        // while (curr && len-count != n) {
+        //     count++;
+        //     prev = curr;
+        //     curr = curr->next;
+        // }
+
+        // if (prev) prev->next = curr->next;
+        // else head = nullptr;
+
+        // delete curr;
+
+        // return head;
+
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* right = dummy;
+        ListNode* left = dummy;
+
+        for (int i = 0; i <= n; i++) right = right->next;
+
+        while (right) {
+            right = right->next;
+            left = left->next;
         }
 
-        if (len == n) {
-            head = head->next;
-            return head;
-        }
+        ListNode* nodeToDeleted = left->next;
+        left->next = left->next->next;
 
-        if (n > len) return head;
+        delete nodeToDeleted;
 
-        ListNode* prev = nullptr;
-        ListNode* curr = head;
-        int count = 0;
-
-        while (curr && len-count != n) {
-            count++;
-            prev = curr;
-            curr = curr->next;
-        }
-
-        if (prev) prev->next = curr->next;
-        else head = nullptr;
-
-        delete curr;
+        head = dummy->next;
+        delete dummy;
 
         return head;
     }
