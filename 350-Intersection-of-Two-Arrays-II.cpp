ce6a80c45a8ca383c899083d729aeaf88570fa2d
Salma Hussien <salmaa.hussien1@gmail.com>
2024-07-02 19:28:43 +0300

    [350] [Time Beats: 81.88%] [Memory Beats: 91.17%] - LeetPush

--- 350-Intersection-of-Two-Arrays-II.cpp
+++ 350-Intersection-of-Two-Arrays-II.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        map<int, int> freq_arr;
+        // map<int, int> freq_arr;
 
-        for (int num: nums1) freq_arr[num]++;
+        // for (int num: nums1) freq_arr[num]++;
+
+        // vector<int> res;
+        // for (int num : nums2) {
+        //     if (freq_arr.count(num) && freq_arr[num] > 0) {
+        //         freq_arr[num]--;
+        //         res.push_back(num);
+        //     }
+        // }
+
+        // return res;
+
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
 
         vector<int> res;
-        for (int num : nums2) {
-            if (freq_arr.count(num) && freq_arr[num] > 0) {
-                freq_arr[num]--;
-                res.push_back(num);
-            }
+        for (int i = 0, j = 0; i < nums1.size() && j < nums2.size(); i++) {
+            
+            while (j < nums2.size() && nums2[j] < nums1[i]) j++;
+
+            if (j == nums2.size()) break;
+
+            if (nums1[i] == nums2[j]) res.push_back(nums2[j++]);
         }
 
         return res;
