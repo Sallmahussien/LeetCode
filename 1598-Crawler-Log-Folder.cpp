074337e292452f6b30f6561444080d86b72a44a3
Salma Hussien <salmaa.hussien1@gmail.com>
2024-07-10 08:31:01 +0300

    [1598] [Time Beats: 100.00%] [Memory Beats: 84.88%] - LeetPush

--- 1598-Crawler-Log-Folder.cpp
+++ 1598-Crawler-Log-Folder.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
-        stack<string> stk;
+        // stack<string> stk;
+
+        // for (string log : logs) {
+        //     if (log == "./") continue;
+        //     else if (log == "../") {
+        //         if (!stk.empty()) stk.pop();
+        //     } else {
+        //         stk.push(log);
+        //     }
+
+        // }
+
+        // return stk.size();
+
+        int res = 0;
 
         for (string log : logs) {
             if (log == "./") continue;
-            else if (log == "../") {
-                if (!stk.empty()) stk.pop();
-            } else {
-                stk.push(log);
-            }
-
+            else if (log == "../") res = max(0, res - 1);
+            else res++;
         }
 
-
-        return stk.size();
+        return res;
     }
 };
