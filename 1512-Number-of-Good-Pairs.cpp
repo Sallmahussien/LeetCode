eadf83c2e2b53ca44d754a9f124d281782a42a01
Salma Hussien <salmaa.hussien1@gmail.com>
2024-07-17 08:18:22 +0300

    [1512] [Time Beats: 100.00%] [Memory Beats: 22.39%] - LeetPush

--- 1512-Number-of-Good-Pairs.cpp
+++ 1512-Number-of-Good-Pairs.cpp
@@ -3,11 +3,17 @@ public:
     int numIdenticalPairs(vector<int>& nums) {
         int res = 0;
 
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] == nums[j]) res ++;
-            }
-        }
+        // for (int i = 0; i < nums.size(); i++) {
+        //     for (int j = i + 1; j < nums.size(); j++) {
+        //         if (nums[i] == nums[j]) res ++;
+        //     }
+        // }
+
+        unordered_map<int, int> freq_arr;
+
+        for (int num : nums) freq_arr[num]++;
+
+        for (auto pair : freq_arr) res += pair.second * (pair.second - 1) / 2;
 
         return res;
     }
