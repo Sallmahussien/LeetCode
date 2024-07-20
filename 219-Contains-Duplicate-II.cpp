591af44dc2d610b6066b0961aebbb5ec6c75753a
Salma Hussien <salmaa.hussien1@gmail.com>
2024-07-20 19:34:24 +0300

    [219] [Time Beats: 83.10%] [Memory Beats: 27.87%] - LeetPush

--- 219-Contains-Duplicate-II.cpp
+++ 219-Contains-Duplicate-II.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        for (int i = 0; i < nums.size(); ++i) {
-            for (int j = i + 1; j < nums.size() && j <= i + k; ++j) {
-                if (nums[i] == nums[j]) {
-                    return true;
-                }
+        unordered_map<int, int> freq_arr;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (!freq_arr.count(nums[i])) freq_arr[nums[i]] = i;
+            else {
+                if (i - freq_arr[nums[i]] <= k) return true;
+                freq_arr[nums[i]] = i;
             }
         }
+
         return false;
     }
 };
