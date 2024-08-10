0eb302ce5d99cdc723a4f34082824986395d9267
Salma Hussien <salmaa.hussien1@gmail.com>
2024-08-10 10:10:08 +0300

    [349] [Time Beats: 26.82%] [Memory Beats: 31.24%] - LeetPush

--- 349-Intersection-of-Two-Arrays.cpp
+++ 349-Intersection-of-Two-Arrays.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        // vector<int> res;
+        // unordered_set<int> unique_nums;
+
+        // sort(nums1.begin(), nums1.end());
+        // sort(nums2.begin(), nums2.end());
+
+        // int i = 0, j = 0;
+
+        // while (i < nums1.size() && j < nums2.size()) {
+        //     if (nums1[i] == nums2[j] && !unique_nums.count(nums1[i])) {
+        //         res.push_back(nums1[i]);
+        //         unique_nums.insert(nums1[i]);
+        //         i++, j++;
+        //     } else if (nums1[i] < nums2[j]) i++;
+        //     else j++;
+        // }
+
+        // return res;
+
+        unordered_map<int, int> freq_arr;
         vector<int> res;
-        unordered_set<int> unique_nums;
 
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
+        for (int num : nums1) freq_arr[num]++;
 
-        int i = 0, j = 0;
-
-        while (i < nums1.size() && j < nums2.size()) {
-            if (nums1[i] == nums2[j] && !unique_nums.count(nums1[i])) {
-                res.push_back(nums1[i]);
-                unique_nums.insert(nums1[i]);
-                i++, j++;
-            } else if (nums1[i] < nums2[j]) i++;
-            else j++;
+        for (int num : nums2) {
+            if (freq_arr.count(num) && freq_arr[num] != 0) {
+                res.push_back(num);
+                freq_arr[num] = 0;
+            }
         }
 
-        return res;
+        return res; 
     }
 };
