558ef78141a73bf6612b0b8e3ec127e30ec6a3a1
Salma Hussien <salmaa.hussien1@gmail.com>
2024-08-14 11:26:48 +0300

    [719] [Time Beats: 5.04%] [Memory Beats: 44.69%] - LeetPush

---
+++ 719-Find-K-th-Smallest-Pair-Distance.cpp
@@ -0,0 +1,46 @@
+class Solution {
+private:
+    int countPairs(vector<int>& nums, int mid) {
+        int count = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i+1; j < nums.size() && nums[j] - nums[i] <= mid; j++) {
+                count++;
+            }
+        }
+    
+        return count;
+    }
+
+public:
+    int smallestDistancePair(vector<int>& nums, int k) {
+        // brute force solution
+        // vector<int> pairs;
+
+        // for (int i = 0; i < nums.size(); i++) {
+        //     for (int j = i+1; j < nums.size(); j++) {
+        //         pairs.push_back(abs(nums[i] - nums[j]));
+        //     }
+        // }
+
+        // sort(pairs.begin(), pairs.end());
+
+        // cout << pairs.size() << endl;
+
+        // return pairs.size() >= k ? pairs[k-1] : 0;
+
+        sort(nums.begin(), nums.end());
+        int n = nums.size() - 1;
+        int l = 0, h = nums[n] - nums[0];
+
+        while (l < h) {
+            int mid = l + (h - l) / 2;
+            int count = countPairs(nums, mid);
+
+            if (count < k) l = mid + 1;
+            else h = mid;
+        }
+
+        return l;
+    }
+};
