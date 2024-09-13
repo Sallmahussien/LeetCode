04f36e95ea3802e971cfcc1d0e5139fd05f53582
Salma Hussien <salmaa.hussien1@gmail.com>
2024-09-13 19:55:26 +0300

    [1310] [Time Beats: 39.75%] [Memory Beats: 8.51%] - LeetPush

--- 1310-XOR-Queries-of-a-Subarray.cpp
+++ 1310-XOR-Queries-of-a-Subarray.cpp
@@ -2,15 +2,28 @@ class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
         vector<int> res;
+        vector<int> prefix_xor;
+        int prefix = 0;
+        for (int n : arr) {
+            prefix ^= n;
+            prefix_xor.push_back(prefix);
+        }
 
-        for (vector<int> query : queries) {
-            int x = 0;
-            for (int i = query[0]; i <= query[1]; i++) {
-                x ^= arr[i];
-            }
+        for (vector<int> query: queries) {
+            int x = prefix_xor[query[1]];
+            if (query[0] > 0) x ^= prefix_xor[query[0]-1];
 
             res.push_back(x);
         }
+
+        // for (vector<int> query : queries) {
+        //     int x = 0;
+        //     for (int i = query[0]; i <= query[1]; i++) {
+        //         x ^= arr[i];
+        //     }
+
+        //     res.push_back(x);
+        // }
 
         return res;
     }
