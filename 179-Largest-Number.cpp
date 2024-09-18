8780a14992c52f43a385935776d4cd497be70ccc
Salma Hussien <salmaa.hussien1@gmail.com>
2024-09-18 14:06:22 +0300

    [179] [Time Beats: 67.11%] [Memory Beats: 85.32%] - LeetPush

--- 179-Largest-Number.cpp
+++ 179-Largest-Number.cpp
@@ -1,26 +1,19 @@
 class Solution {
 private:
-    string concatNums (int num1, int num2) {
-        return to_string(num1) + to_string(num2);
+    static bool compare (int a, int b) {
+        string strA = to_string(a);
+        string strB = to_string(b);
+        return strA + strB > strB + strA;
     }
 
 public:
     string largestNumber(vector<int>& nums) {
-        for (int i = 1; i < nums.size(); i++) {
-            for (int j = i; j > 0; j--) {
-                string concat1 = concatNums(nums[j], nums[j-1]);
-                string concat2 = concatNums(nums[j-1], nums[j]);
-                if (concat1 < concat2) break;
-                else {
-                    swap(nums[j], nums[j-1]);
-                }
+        sort(nums.begin(), nums.end(), compare);
 
-            }
-        }
+        if (nums[0] == 0) return \0\;
 
         string res = \\;
         for (int num : nums) {
-            if (res == \0\ && num == 0) break;
             res += to_string(num);
         }
 
