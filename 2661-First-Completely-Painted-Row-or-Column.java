c11b9388e1d8f84876024bcb9eb2129ad98c10d3
Salma Hussien <salmaa.hussien1@gmail.com>
2025-01-20 22:00:03 +0200

    [2661] [Time Beats: 25.96%] [Memory Beats: 12.50%] - LeetPush

---
+++ 2661-First-Completely-Painted-Row-or-Column.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int firstCompleteIndex(int[] arr, int[][] mat) {
+        Map<Integer, int[]> numWithPos = new HashMap<>();
+
+        int numRows = mat.length;
+        int numCols = mat[0].length;
+
+        System.out.println(numRows);
+        System.out.println(numCols);
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                numWithPos.put(mat[i][j], new int[]{i, j});
+            }
+        }
+
+        int[] rows = new int[numRows + 1];
+        int[] cols = new int[numCols + 1];
+
+        int res = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            int n = arr[i];
+            int rowIdx = numWithPos.get(n)[0];
+            int colIdx = numWithPos.get(n)[1];
+
+            rows[rowIdx]++;
+            if (rows[rowIdx] ==  numCols) {
+                res = i;
+                break;
+            }
+
+            cols[colIdx]++;
+            if (cols[colIdx] == numRows) {
+                res = i;
+                break;
+            }
+        }
+
+        return res;
+    }
+}
