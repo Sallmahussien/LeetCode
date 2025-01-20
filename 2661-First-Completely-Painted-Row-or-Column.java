4878b20d77c6443027c8f98dab3b412d38817a72
Salma Hussien <salmaa.hussien1@gmail.com>
2025-01-20 22:02:25 +0200

    [2661] [Time Beats: 45.19%] [Memory Beats: 50.96%] - LeetPush

--- 2661-First-Completely-Painted-Row-or-Column.java
+++ 2661-First-Completely-Painted-Row-or-Column.java
@@ -5,17 +5,14 @@ class Solution {
         int numRows = mat.length;
         int numCols = mat[0].length;
 
-        System.out.println(numRows);
-        System.out.println(numCols);
-
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numCols; j++) {
                 numWithPos.put(mat[i][j], new int[]{i, j});
             }
         }
 
-        int[] rows = new int[numRows + 1];
-        int[] cols = new int[numCols + 1];
+        int[] rows = new int[numRows];
+        int[] cols = new int[numCols];
 
         int res = -1;
 
