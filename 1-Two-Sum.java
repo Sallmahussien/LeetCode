d19be65fe017a5b214aeccd4e66666a18927bb91
Salma Hussien <salmaa.hussien1@gmail.com>
2025-02-07 23:47:00 +0200

    [1] [Time Beats: 98.89%] [Memory Beats: 44.53%] - LeetPush

--- 1-Two-Sum.java
+++ 1-Two-Sum.java
@@ -1,17 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> numsMap = new HashMap<>();
-
-
+        Map<Integer, Integer> freqArr = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int secondNum = target - nums[i];
-            if (numsMap.containsKey(secondNum)) {
-                return new int[] {i, numsMap.get(secondNum)};
+            if (freqArr.containsKey(target - nums[i])) {
+                return new int[] {freqArr.get(target - nums[i]), i};
             }
-
-            numsMap.put(nums[i], i);
+            freqArr.put(nums[i], i);
         }
 
-        return new int[] {};
+        return new int[]{};      
     }
 }
