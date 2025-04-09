f426913fefa516620baca34b1f81d2a7092d05dc
Salma Hussien <salmaa.hussien1@gmail.com>
2025-04-09 21:41:00 +0200

    [3375] [Time Beats: 82.19%] [Memory Beats: 89.04%] - LeetPush

--- Minimum-Operations-to-Make-Array-Values-Equal-to-K.java
+++ Minimum-Operations-to-Make-Array-Values-Equal-to-K.java
@@ -1,21 +1,14 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        Set<Integer> set = new HashSet<>();
+        int ans = 0;
 
         for (int num : nums) {
             if (num < k) return -1;
-            maxHeap.offer(num);
-        }
+            
+            if (set.contains(num) || num == k) continue;
 
-        int ans = 0;
-
-        while (!maxHeap.isEmpty() && maxHeap.peek() != k) {
-            int maxVal = maxHeap.poll();
-
-            while (!maxHeap.isEmpty() && maxHeap.peek() == maxVal) {
-                maxHeap.poll();
-            }
-
+            set.add(num);
             ans++;
         }
 
