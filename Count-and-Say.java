7452ae5fdc2d024c170138ec5f813385ed3d7843
Salma Hussien <salmaa.hussien1@gmail.com>
2025-04-18 18:47:00 +0200

    [38] [Time Beats: 38.96%] [Memory Beats: 36.71%] - LeetPush

--- Count-and-Say.java
+++ Count-and-Say.java
@@ -3,16 +3,15 @@ class Solution {
         StringBuilder str = new StringBuilder(\1\);
 
         for (int i = 1; i < n; i++) {
-            int l = 0;
             int len = str.length();
             int count = 0;
             StringBuilder sb =  new StringBuilder();
 
-            for (int r = 0; r < len; r++) {
+            for (int j = 0; j < len; j++) {
                 count++;
 
-                if (r == len - 1 || str.charAt(r) != str.charAt(r + 1)) {
-                    sb.append(count).append(str.charAt(r));
+                if (j == len - 1 || str.charAt(j) != str.charAt(j + 1)) {
+                    sb.append(count).append(str.charAt(j));
                     count = 0;
                 }
             }
